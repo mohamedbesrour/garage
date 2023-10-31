f4d7d8fa17fc4d2972895958017a1470a2b4f901
mohamedbesrour <118281616+mohamedbesrour@users.noreply.github.com>
2023-10-31 11:50:12 +0100

    section commentaire terminer

---
+++ client/src/components/EditTodo.js
@@ -0,0 +1,90 @@
+import React, { Fragment, useState } from "react";
+
+const EditTodo = ({ commentaire }) => {
+  const [description, setDescription] = useState(commentaire.description);
+
+  //Edit description function
+  const updateDescription = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch(
+        `http://localhost:5000/todos/${commentaire.commentaire_id}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
+        }
+      );
+
+      window.location = "/";
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+  return (
+    <Fragment>
+      <button
+        type="button"
+        className="btn btn-warning"
+        data-toggle="modal"
+        data-target={`#id${commentaire.commentaire_id}`}
+      >
+        Edit
+      </button>
+
+      {/* id = id10    pour rendre le modal unique*/}
+      <div
+        className="modal"
+        id={`id${commentaire.commentaire_id}`}
+        onClick={() => setDescription(commentaire.description)} //modifie sur la même ligne
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">modifier le commentaire</h4>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                onClick={() => setDescription(commentaire.description)} //modifie sur la même ligne
+              >
+                &times;
+              </button>
+            </div>
+
+            <div className="modal-body">
+              <input
+                type="text"
+                className="form-control"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-warning"
+                data-dismiss="modal"
+                onClick={(e) => updateDescription(e)}
+              >
+                Modifier
+              </button>
+              <button
+                type="button"
+                className="btn btn-danger"
+                data-dismiss="modal"
+                onClick={() => setDescription(commentaire.description)} //modifie sur la même ligne
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default EditTodo;
---
+++ client/src/components/ListTodos.js
@@ -0,0 +1,77 @@
+import React, { Fragment, useEffect, useState } from "react";
+
+import EditTodo from "./EditTodo";
+
+const ListTodos = () => {
+  const [todos, setTodos] = useState([]);
+
+  // pour supprimer un commentaire
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE",
+      });
+
+      setTodos(
+        todos.filter((commentaire) => commentaire.commentaire_id !== id)
+      ); // désaffiche le commentaire qui a été supprimé en interne
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  // pour lister tout les commentaires
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/todos");
+      const jsonData = await response.json(); //await pour dire d'attendre
+
+      setTodos(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  return (
+    <Fragment>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Commentaire</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/*<tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>  */}
+          {todos.map((commentaire) => (
+            <tr key={commentaire.commentaire_id}>
+              <td>{commentaire.description}</td>
+              <td>
+                <EditTodo commentaire={commentaire} />
+              </td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(commentaire.commentaire_id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+export default ListTodos;
