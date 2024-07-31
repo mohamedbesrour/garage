35d4a004c9b206a92910ec900450979b29cd5a7a
mohamedbesrour <m.besrour@yahoo.com>
2024-07-31 16:21:32 +0200

    upd

--- client/src/navigation/Footer.js
+++ client/src/navigation/Footer.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { FaFacebook, FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
 import "../style/outils/footer.css";
-// actualisation
 function Footer() {
   return (
     <div>
